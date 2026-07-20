b4423589da38144f0c7b57a826702e7b30fe7707
sumesh <sumesh@frontlinetech.in>
2026-07-20 16:58:59 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Header from "./components/layouts/header.jsx";
-import Footer from "./components/layouts/footer.jsx";
+// import Footer from "./components/layouts/footer.jsx";
 import Hero from "./components/landing-page/hero.jsx";
 import About from "./components/landing-page/about.jsx";
 import Products from "./components/landing-page/products.jsx";
 import Industries from "./components/landing-page/industries.jsx";
 import Projects from "./components/landing-page/projects.jsx";
 import Contact from "./components/landing-page/contact.jsx";
+import Footer from "./components/layouts/footer.jsx";
 import Bottomnav from "./components/common/bottom-nav.jsx";
 import Floating from "./components/common/float.jsx";
 
